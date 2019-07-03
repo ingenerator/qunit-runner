7bd1d36e7bd0771ad1928f09d6c88e36ca6b4f06
acoulton <andrew@ingenerator.com>
2019-07-03 12:11:42 +0100

    Add simple integration tests for passing and failing test suite

---
+++ tests/integration-test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FAILED_SUITES=""
+
+if [ $# -eq 1 ]
+then
+  IMAGE_TAG=$1
+else
+  echo "Specify image and tag to run as first argument e.g. selftest.sh eu.gcr.io/ingenerator-ci/qunit-runner:0.1"
+  exit 1
+fi
+
+
+run_test_suite() {
+  local test_suite=$1
+  local expect_result=$2
+  local actual_result=''
+  local newline=$'\n'
+
+  echo ""
+  echo "> Running test suite $test_suite, expecting exit code $expect_result"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+  set +e
+  docker run --cap-add=SYS_ADMIN --rm -it -v $DIR:/workspace $IMAGE_TAG test $test_suite
+  actual_result=$?
+  set -e
+
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+  if [ $actual_result -eq $expect_result ]
+  then
+    echo "[✓] $test_suite OK"
+  else
+    echo "[✗] $test_suite - expected exit code $expect_result, got $actual_result"
+    FAILED_SUITES="$FAILED_SUITES - $test_suite$newline"
+  fi
+}
+
+run_test_suite integration/explicit-suite/qunit-passing.html 0
+run_test_suite integration/explicit-suite/qunit-failing.html 1
+
+echo ""
+echo ""
+if [ -z "$FAILED_SUITES" ]
+then
+  echo "✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓"
+  echo "All tests OK"
+  echo "✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓"
+else
+  echo "✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗"
+  echo "Some suites failed:"
+  echo "$FAILED_SUITES"
+  echo "✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗"
+fi
